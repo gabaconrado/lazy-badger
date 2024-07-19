d09850a8d6205af8acd76eedf781b6ff4706c612
Gabriel Conrado <gabaconrado@gmail.com>
2024-07-18 23:26:06 -0400

    update cache actions + add create tag script

---
+++ scripts/dev/create-tag.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This script creates a tag and pushes to the repository
+#
+
+MAIN_BRANCH="main"
+
+tag="${1}"
+
+if [ -z "${tag}" ]; then
+    echo "Missing tag parameter"
+fi
+
+current_branch="$(git branch --show-current)"
+
+if [ "${current_branch}" != "${MAIN_BRANCH}" ]; then
+    echo "Tags must be created on the '${MAIN_BRANCH}'. Current is '${current_branch}'"
+fi
+
+git tag "${tag}" -a
